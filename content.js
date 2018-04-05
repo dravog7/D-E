93cb4d36845e3b4ec8bd1ffa30705f481fc1876f
dravog7 <dravog77@gmail.com>
2018-04-05 20:08:44 +0530

    adding pop up

--- content.js
+++ content.js
@@ -1,13 +1,46 @@
-function popup()
+function popup(value)
 //generate a popup div for the data
 {
-    return;
+    div=document.createElement("div");
+    div.style.position="fixed";
+    div.style.top="50%";
+    div.style.left="50%";
+    div.style.height="25%";
+    div.style.width="25%";
+    div.style.border="1px black rigid";
+
+    close=document.createElement("span");
+    close.innerText="X";
+    close.addEventListener("click",(event)=>
+    {
+        event.target.parentElement.remove();
+    });
+    close.position="relative";
+    close.left="99%";
+    close.bottom="99%";
+    
+    text=document.createElement("textarea");
+    text.value=value;
+    text.style.height="95%";
+    text.style.width="100%";
+
+    div.appendChild(close);
+    div.appendChild(text);
+    document.body.appendChild(div);
 }
+
 function pass()
 //an html div to input password
 {
     return;
 }
+
+function errorhandle(err)
+//handles all errors
+{
+    console.error(err);
+}
+
 function digest(key,callback) 
 //converts ArrayBuffer of any size to 256 bit by hashing SHA256
 //generates the cryptoKey object using the 256 bit hashed
@@ -39,16 +72,20 @@ document.addEventListener("mousedown", function(event)
 //stores which element was clicked last time
 {
     //right click
-    if(event.button == 2) { 
+    if(event.button == 2)
+    { 
         targetc=event.target;
-        targetc.val=(a=0)=>{
+        targetc.val=(a=0)=>
+        {
             //gmail uses editable div, as a fix uses this function
             if(a)
             {
-                if(targetc.tagName=="DIV")
+                /*if(targetc.tagName=="DIV")
                     targetc.innerText=a;
                 else
                     targetc.value=a;
+                */
+               popup(a);
             }
             else
             {
@@ -61,6 +98,12 @@ document.addEventListener("mousedown", function(event)
     }
 }, true);
 
+/*
+
+The main part of the code starts here. By adding an event listener to messages from main.js.
+These msgs are commands whether to encrypt or decrypt
+
+*/
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 //recieve the msg from main.js telling whether to encrypt/decrypt 
 {
@@ -82,7 +125,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
                 ).then((encoded)=>
                 {
                     targetc.val(btoa(new Uint8Array(encoded).toString()));//convert the result to array and to base64
-                }).catch((err)=>{console.error(err);});
+                }).catch((err)=>{errorhandle(err)});
             });
     }
     if(request.id=="decrypt")
@@ -101,12 +144,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
                     key,
                     new Uint8Array(atob(targetc.val()).split(",").map(Number)) //decode base64, convert to unt8Array
                 ).then((encoded)=>
-                {
-                    targetc.val(new TextDecoder().decode(encoded)); //decode the output
-                }).catch((error)=>
-                {
-                    console.error(error);
-                });
+                    {
+                        targetc.val(new TextDecoder().decode(encoded)); //decode the output
+                    }
+                ).catch((err)=>{errorhandle(err)});
             });
     }
 });
